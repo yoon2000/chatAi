54037bdc9f7d5d109d748b33d57ead556fdb9a6f
yoon2000 <55117130+yoon2000@users.noreply.github.com>
2025-01-06 10:01:08 +0900

    first commit

---
+++ src/main/java/com/ll/chatAi/ChatAiApplication.java
@@ -0,0 +1,13 @@
+package com.ll.chatAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/chatAi/ChatAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.chatAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
