96f1ded7facbcb2b9034e7136fc958e1931b1c11
yoon2000 <55117130+yoon2000@users.noreply.github.com>
2025-01-06 10:27:04 +0900

    connect Ai by api

---
+++ src/main/java/com/ll/chatAi/ChatController.java
@@ -0,0 +1,31 @@
+package com.ll.chatAi;
+
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequestMapping("/api/v1/chat")
+@RestController
+public class ChatController {
+    private final OpenAiChatModel openAiChatModel;
+
+    public ChatController(OpenAiChatModel openAiChatModel) {
+        this.openAiChatModel = openAiChatModel;
+    }
+
+    @GetMapping("/ai")
+    public Map<String, String> chat(@RequestBody String message) {
+        Map<String, String> responses = new HashMap<>();
+
+        String openAiResponse = openAiChatModel.call(message);
+        responses.put("openAi - ChatGPT 4 응답", openAiResponse);
+
+        return responses;
+    }
+
+}
