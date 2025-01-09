edc7503a2d31dc8b27672a80f552c98e8c78c870
yoon2000 <55117130+yoon2000@users.noreply.github.com>
2025-01-09 16:41:00 +0900

    update ArticleController & list.html

--- src/main/java/com/ll/chatAi/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/chatAi/domain/article/article/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.ll.chatAi.domain.article.article.controller;
 import com.ll.chatAi.domain.article.article.entity.Article;
 import com.ll.chatAi.domain.article.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,22 +16,38 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/article")
 @RequiredArgsConstructor
+@Slf4j // console에 로그를 찍어줌 (디버깅용)
 public class ArticleController {
     private final ArticleService articleService;
+
     @GetMapping("/list")
     public String list(
-            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "kwType", defaultValue = "") List<String> kwTypes,
+            @RequestParam(defaultValue = "") String kw,
+            @RequestParam(defaultValue = "0") int page,
             Model model
     ) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("id"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+
+        Map<String, Boolean> kwTypesMap = kwTypes
+                .stream()
+                .collect(Collectors.toMap(
+                        kwType -> kwType,
+                        kwType -> true
+                ));
+        log.debug("kwTypesMap: {}", kwTypesMap);
         Page<Article> itemsPage = articleService.search(pageable);
         model.addAttribute("itemsPage", itemsPage);
+        model.addAttribute("kwTypesMap", kwTypesMap);
+
         return "article/list";
     }
 }
