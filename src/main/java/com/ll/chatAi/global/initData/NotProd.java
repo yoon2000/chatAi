afe660ce9547b222b903a93f6fb3f385dc488629
yoon2000 <55117130+yoon2000@users.noreply.github.com>
2025-01-09 16:27:40 +0900

    update list.html

--- src/main/java/com/ll/chatAi/global/initData/NotProd.java
+++ src/main/java/com/ll/chatAi/global/initData/NotProd.java
@@ -1,77 +1,78 @@
 package com.ll.chatAi.global.initData;
 
+
 import com.ll.chatAi.domain.article.article.entity.Article;
-import com.ll.chatAi.domain.article.article.repository.ArticleRepository;
 import com.ll.chatAi.domain.article.article.service.ArticleService;
-import com.ll.chatAi.domain.chat.chatMessage.service.ChatMessageService;
-import com.ll.chatAi.domain.chat.chatRoom.entity.ChatRoom;
-import com.ll.chatAi.domain.chat.chatRoom.service.ChatRoomService;
 import com.ll.chatAi.domain.member.member.entity.Member;
 import com.ll.chatAi.domain.member.member.service.MemberService;
-import org.springframework.boot.ApplicationArguments;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.IntStream;
 
-@Configuration
 @Profile("!prod")
+@Configuration
+@RequiredArgsConstructor
 public class NotProd {
+    @Autowired
+    @Lazy
+    private NotProd self;
+    private final MemberService memberService;
+    private final ArticleService articleService;
 
     @Bean
-    public ApplicationRunner initNotProd(
-            ChatRoomService chatRoomService,
-            ChatMessageService chatMessageService,
-            MemberService memberService,
-            ArticleService articleService,
-            ArticleRepository articleRepository
-    ) {
-        return new ApplicationRunner() {
-            @Override
-            @Transactional
-            public void run(ApplicationArguments args) throws Exception {
-                ChatRoom chatRoom1 = chatRoomService.make("공부");
-                ChatRoom chatRoom2 = chatRoomService.make("식사");
-                ChatRoom chatRoom3 = chatRoomService.make("휴식");
-
-                IntStream.rangeClosed(1, 10).forEach(num -> {
-                    chatMessageService.write(chatRoom1, "홍길동", "공부 메시지" + num);
-                });
-
-                Member member1 = memberService.join("user1", "1234").getData();
-                Member member2 = memberService.join("user2", "1234").getData();
-                Member member3 = memberService.join("user3", "1234").getData();
-
-                Article article1 = articleService.write(member1.getId(), "제목1", "내용1").getData();
-                Article article2 = articleService.write(member2.getId(), "제목2", "내용2").getData();
-
-                Article article3 = articleService.write(member3.getId(), "제목3", "내용3").getData();
-                Article article4 = articleService.write(member3.getId(), "제목4", "내용4").getData();
-
-                article1.addComment(member1, "댓글1");
-                article1.addComment(member1, "댓글2");
-
-                article2.addComment(member1, "댓글3");
-                article2.addComment(member1, "댓글4");
-                article2.addComment(member1, "댓글5");
-
-                article3.addComment(member1, "댓글5");
-                article3.addComment(member1, "댓글6");
-                article3.addComment(member1, "댓글7");
-                article3.addComment(member1, "댓글8");
-                article3.addComment(member1, "댓글9");
-                article3.addComment(member1, "댓글10");
-                article3.addComment(member1, "댓글11");
-                article3.addComment(member1, "댓글12");
-
-                article1.addTag("자바");
-                article1.addTag("백엔드");
-                article2.addTags("프레임워크", "스프링부트");
-                article4.addTags("자바", "스프링부트");
-            }
+    public ApplicationRunner initNotProdData() {
+        return args -> {
+            self.work1();
         };
+    }
+
+    @Transactional
+    public void work1() {
+        Member member1 = memberService.join("user1", "1234").getData();
+        Member member2 = memberService.join("user2", "1234").getData();
+        Member member3 = memberService.join("user3", "1234").getData();
+
+        Article article1 = articleService.write(member1.getId(), "제목1", "내용1").getData();
+        Article article2 = articleService.write(member1.getId(), "제목2", "내용2").getData();
+
+        Article article3 = articleService.write(member2.getId(), "제목3", "내용3").getData();
+        Article article4 = articleService.write(member2.getId(), "제목4", "내용4").getData();
+
+        article1.addComment(member1, "댓글1");
+        article1.addComment(member1, "댓글2");
+
+        article2.addComment(member1, "댓글3");
+        article2.addComment(member1, "댓글4");
+        article2.addComment(member1, "댓글5");
+
+        article3.addComment(member1, "댓글5");
+        article3.addComment(member1, "댓글6");
+        article3.addComment(member1, "댓글7");
+        article3.addComment(member1, "댓글8");
+        article3.addComment(member1, "댓글9");
+        article3.addComment(member1, "댓글10");
+        article3.addComment(member1, "댓글11");
+        article3.addComment(member1, "댓글12");
+
+        article1.addTag("자바");
+        article1.addTag("백엔드");
+        article2.addTags("프레임워크", "스프링부트");
+
+        article4.addTags("자바", "스프링부트");
+
+        IntStream.rangeClosed(5, 120).forEach(
+                i -> {
+                    String title = "제목" + i;
+                    String body = "내용" + i;
+                    articleService.write(member2.getId(), title, body);
+                }
+        );
     }
 }
